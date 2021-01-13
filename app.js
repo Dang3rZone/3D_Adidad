c7acccfb328b2d927fa863797035855014eda6e3
Andresrosalespinzon <andresrosalespinzon@gmail.com>
2021-01-13 20:59:37 +0100

    added perspective to body, moved the eventlist to the card and change how sensitive it is

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
+
+card.addEventListener('mousemove', (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 15;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
